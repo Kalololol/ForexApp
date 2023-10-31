2b181548c87dcaa5f297b96567bd7f389a4c7c80
Karol kalololol <karol.binkowski92@gmail.com>
2023-10-31 22:08:07 +0100

    add test and correct method

---
+++ src/main/java/test/CurrencyDownloadApiTest.java
@@ -0,0 +1,10 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+public class CurrencyDownloadApiTest {
+    private CurrencyDownloadApiTest currencyDownloadApiTest = new CurrencyDownloadApiTest();
+
+    @Test
+    public void getCurrencyTest(){
+        Assertions.
+    }
+}
