44148476152466d682721502ce9ad05453fa989e
Karol kalololol <karol.binkowski92@gmail.com>
2023-11-03 09:37:46 +0100

    create Test

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,16 +1,21 @@
 package org.example;
 
 
+import api.CurrencyDownloadApi;
+import model.Currency;
+
 public class Main {
     public static void main(String[] args) {
-//        CurrencyDownloadApi api = new CurrencyDownloadApi();
-//        Currency currency = new Currency();
+        CurrencyDownloadApi api = new CurrencyDownloadApi();
+        Currency currency = new Currency();
 //        System.out.println(api.getCurrency("usd", "2023-10-31"));
 //        api.getCurrency("usd", "2023-09-29");
 //        System.out.println(api.getCurrencyAcctualCourse("usd"));
 
         String result = "{\"table\":\"A\",\"currency\":\"dolar amerykański\",\"code\":\"USD\",\"rates\":[{\"no\":\"211/A/NBP/2023\",\"effectiveDate\":\"2023-10-31\",\"mid\":4.1684}]}";
 
+        String result1 = api.getCurrency("usd","2023-11-03");
+        System.out.println(result1);
     }
     }
 
--- src/main/java/test/CurrencyTest.java
+++ src/main/java/test/CurrencyTest.java
@@ -1,14 +1,24 @@
 import api.CurrencyDownloadApi;
+import logic.JsonMapper;
+import model.Currency;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+
+import java.util.Date;
+import java.util.function.BooleanSupplier;
+
 public class CurrencyTest {
     private CurrencyDownloadApi currencyDownloadApi = new CurrencyDownloadApi();
+    private JsonMapper jsonMapper = new JsonMapper();
 
+    String jsonCourseUsd = currencyDownloadApi.getCurrency("usd","2023-11-03");
+    String jsonResult = "{\"table\":\"A\",\"currency\":\"dolar ameryka�ski\",\"code\":\"USD\",\"rates\":[{\"no\":\"211/A/NBP/2023\",\"effectiveDate\":\"2023-11-02\",\"mid\":4.1963}]}";
+
+
+    Currency currencyTest = new Currency(new Date(2023-10-31), "usd", 4.1963);
     @Test
-    public void getCurrencyTest(){
-//        double test = currencyDownloadApi.getCurrency("usd", "2023-10-31");
-//        double result = 4.1684;
-//
-//        Assertions.assertEquals(result, test );
+    public void jsonMapperTest(){
+        Assertions.assertEquals(jsonMapper.mapToCurrency(jsonResult), currencyTest);
     }
 }
 
